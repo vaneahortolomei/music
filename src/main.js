3199cca9c53aeba49a43c4b1f0f273244c7f1b37
Ваня Хортоломей <hortolomei1989@gmail.com>
2022-08-24 15:16:40 +0300

    music - add new package for validation and config

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
-import { createApp } from "vue";
-import { createPinia } from "pinia";
+import {createApp} from "vue";
+import {createPinia} from "pinia";
 
+import VeeValidatePlugin from "./includes/validation.js";
 import App from "./App.vue";
 import router from "./router";
 
@@ -10,5 +11,5 @@ const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
-
+app.use(VeeValidatePlugin);
 app.mount("#app");
